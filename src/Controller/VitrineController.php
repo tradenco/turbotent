40b1ecaf5e8d844fc065344491875cc221094157
frederic poesson <f.poesson@nouveauxterritoires.fr>
2024-06-28 17:54:07 +0200

    second commit

--- src/Controller/VitrineController.php
+++ src/Controller/VitrineController.php
@@ -2,16 +2,11 @@
 
 namespace App\Controller;
 
+use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Validator\Constraints\Email;
-use Symfony\Component\Validator\Constraints\Length;
-use Symfony\Component\Validator\Constraints\NotBlank;
 
 #[route('/vitrine', name: 'app.vitrine.')]
 class VitrineController extends AbstractController
@@ -25,25 +20,15 @@ class VitrineController extends AbstractController
     #[Route('/contact', name: 'contact')]
     public function contact(Request $request): Response
     {
-        $form = $this->createFormBuilder()
-            ->add('name', TextType::class, [
-                    'constraints' => [new NotBlank(), new Length(min: 2, max: 100)]
-                ]
-            )
-            ->add('email', EmailType::class, [
-                'attr' => ['placeholder' => 'Email'],
-                'constraints' => [new NotBlank(), new Email()]
-            ])
-            ->add('message', TextareaType::class, [
-                'attr' => ['placeholder' => 'Message'],
-                'constraints' => [new NotBlank(), new Length(min: 5)]
-            ])
-            ->getForm();
-
+        $form = $this->createForm(ContactType::class);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            dump($form->getData());
+
+            if (str_contains($request->headers->get('Accept'), 'text/vnd.turbo-stream.html')) {
+                return new Response($this->renderView('partials/_success.stream.html.twig'));
+            }
+
             $this->addFlash('success', 'je vous recontacte très bientôt');
             return $this->redirectToRoute(route: 'app.vitrine.index', status: Response::HTTP_SEE_OTHER);
         }
@@ -67,11 +52,7 @@ class VitrineController extends AbstractController
 
         // troisième methode
         $response = new Response(null, $form->isSubmitted() && !$form->isValid() ? Response::HTTP_UNPROCESSABLE_ENTITY : Response::HTTP_OK);
-        return $this->render('vitrine/contact.html.twig', [
-            'form' => $form->createView()
-        ],
-            $response
-        );
+        return $this->render('vitrine/contact.html.twig', ['form' => $form->createView()], $response);
     }
 
     #[Route('/about', name: 'about')]
---
+++ src/Form/ContactType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class ContactType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'constraints' => [new NotBlank(), new Length(min: 2, max: 100)]
+            ])
+            ->add('email', EmailType::class, [
+                'attr' => ['placeholder' => 'Email'],
+                'constraints' => [new NotBlank(), new Email()]
+            ])
+            ->add('message', TextareaType::class, [
+                'attr' => ['placeholder' => 'Message'],
+                'constraints' => [new NotBlank(), new Length(min: 5)]
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
